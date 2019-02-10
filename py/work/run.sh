4dc83758247ec9da2e76cb7eb63e776f9edb3055
Joy Diamond <python.gem@gmail.com>
2019-02-10 14:53:57 -0500

    Rename "Grow/1/ => Grow/2/"

--- py/work/run.sh
+++ py/work/run.sh
@@ -168,7 +168,7 @@ do
     fi
 
     if [ $show = 5 ]; then
-        if (cd ../Other/Grow/1; python $Main_py) <$tmp1 >&$tmp3; then
+        if (cd ../Other/Grow/2; python $Main_py) <$tmp1 >&$tmp3; then
             :
         fi
 
